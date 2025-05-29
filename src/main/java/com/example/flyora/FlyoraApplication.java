2f94619cdb8d0df4b69544e16f130c4ecae2614e
Huntrot <captainparrot13042005@gmail.com>
2025-05-29 21:31:04 +0700

    set up Spring Boot

---
+++ src/main/java/com/example/flyora/FlyoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flyora/FlyoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
